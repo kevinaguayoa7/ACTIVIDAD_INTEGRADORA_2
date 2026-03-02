61aca50b8ae9cf7e38da4ff7686dafaed325e124
kevinaguayoa7 <ke.aguayo@eston.ecotec.edu.ec>
2026-03-01 23:45:39 -0500

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\uga2b\Desktop\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\uga2b\Desktop\actividad integradora 2 Joselyn apps moviles\mini_app_usuario"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
